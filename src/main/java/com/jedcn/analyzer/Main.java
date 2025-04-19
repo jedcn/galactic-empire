a70a91984a8d954d340b2d8fe314a2358fb7c4cc
Jed Northridge <git@jedcn.com>
2025-04-19 19:33:58 -0400

    Create basic java files and maven infrastructure

---
+++ src/main/java/com/jedcn/analyzer/Main.java
@@ -0,0 +1,9 @@
+package com.jedcn.analyzer;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Galactic Empire Analyzer");
+        
+        // TODO: Implement file analysis logic
+    }
+}
---
+++ src/test/java/com/jedcn/analyzer/MainTest.java
@@ -0,0 +1,12 @@
+package com.jedcn.analyzer;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class MainTest {
+    @Test
+    public void testMainInitialization() {
+        // Basic test to verify test setup works
+        assertTrue(true);
+    }
+}
